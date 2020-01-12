dd1d0b7633eae4024c6375585384c52745c2324f
Max Verba <maxwillow93@gmail.com>
2020-01-12 15:19:18 +0200

    Updates

---
+++ precache-manifest.ef2d63ced3f80ee1e95143159f382bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8a4d5eeac8daee11a4874305b53fdb",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "078dad1a78d45c527950",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.796dc7cd.chunk.css"
+  },
+  {
+    "revision": "af099d58bf64acac628b",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.6e33bcfa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.6e33bcfa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "078dad1a78d45c527950",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.7af37162.chunk.js"
+  },
+  {
+    "revision": "e9e327586151e37acc36",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.56e6497a.js"
+  }
+]);
